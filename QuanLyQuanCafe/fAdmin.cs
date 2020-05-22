7c1653818a17837d94d0b72ae01a8ed206752325
Dog99 <quangsoi99@gmail.com>
2020-05-22 22:28:37 +0700

    Feature/ Excel

--- QuanLyQuanCafe/fAdmin.cs
+++ QuanLyQuanCafe/fAdmin.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using excel = Microsoft.Office.Interop.Excel;
 
 namespace QuanLyQuanCafe
 {
@@ -351,13 +352,7 @@ namespace QuanLyQuanCafe
             ResetPassword(userName);
         }
 
-        private void btnReport_Click(object sender, EventArgs e)
-        {
-            DateTime checkIn = Convert.ToDateTime(dtpkFromDate.Value);
-            DateTime checkOut = Convert.ToDateTime(dtpkToDate.Value);
-            fReport fReport = new fReport(checkIn, checkOut);
-            fReport.Show();
-        }
+     
 
         #endregion
 
@@ -372,6 +367,130 @@ namespace QuanLyQuanCafe
 
         }
 
-       
+        #region Report
+        private void btnReport_Click(object sender, EventArgs e)
+        {
+            DateTime checkIn = Convert.ToDateTime(dtpkFromDate.Value);
+            DateTime checkOut = Convert.ToDateTime(dtpkToDate.Value);
+            fReport fReport = new fReport(checkIn, checkOut);
+            fReport.Show();
+        }
+
+        #endregion
+
+        #region Excel
+        public void ExportExcel(DataTable tb, string sheetname)
+        {
+            excel.Application oExcel = new excel.Application();
+            excel.Workbooks oBooks;
+            excel.Sheets oSheets;
+            excel.Workbook oBook;
+            excel.Worksheet oSheet;
+            //Tạo mới một Excel WorkBook 
+            oExcel.Visible = true;
+            oExcel.DisplayAlerts = false;
+            oExcel.Application.SheetsInNewWorkbook = 1;
+            oBooks = oExcel.Workbooks;
+            oBook = (excel.Workbook)(oExcel.Workbooks.Add(Type.Missing));
+            oSheets = oBook.Worksheets;
+            oSheet = (excel.Worksheet)oSheets.get_Item(1);
+            oSheet.Name = sheetname;
+
+            // Tạo phần đầu nếu muốn
+            excel.Range head = oSheet.get_Range("A1", "C1");
+            head.MergeCells = true;
+            head.Value2 = "Báo Cáo Tài Chính Theo Tháng";
+            head.Font.Bold = true;
+            head.Font.Name = "Tahoma";
+            head.Font.Size = "18";
+            head.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+
+            // Tạo tiêu đề cột 
+            excel.Range cl1 = oSheet.get_Range("A3", "A3");
+            cl1.Value2 = "Tên Bàn";
+            cl1.ColumnWidth = 10;
+
+            excel.Range cl2 = oSheet.get_Range("B3", "B3");
+            cl2.Value2 = "Giảm Giá";
+            cl2.ColumnWidth = 20.0;
+
+            excel.Range cl3 = oSheet.get_Range("C3", "C3");
+            cl3.Value2 = "Tổng Số Tiền";
+            cl3.ColumnWidth = 20;
+
+            excel.Range cl4 = oSheet.get_Range("D3", "D3");
+            cl4.Value2 = "Ngày Vào";
+            cl4.ColumnWidth = 20;
+
+            excel.Range cl5 = oSheet.get_Range("E3", "E3");
+            cl5.Value2 = "Ngày Ra";
+            cl5.ColumnWidth = 20;
+
+
+            excel.Range rowHead = oSheet.get_Range("A3", "E3");
+            rowHead.Font.Bold = true;
+            // Kẻ viền
+            rowHead.Borders.LineStyle = Microsoft.Office.Interop.Excel.Constants.xlSolid;
+            // Thiết lập màu nền
+            rowHead.Interior.ColorIndex = 15;
+            //rowHead.HorizontalAlignment = excel.XlHAlign.xlHAlignCenter;
+            // Tạo mảng đối tượng để lưu dữ toàn bồ dữ liệu trong DataTable,
+            // vì dữ liệu được được gán vào các Cell trong Excel phải thông qua object thuần.
+            object[,] arr = new object[tb.Rows.Count, tb.Columns.Count];
+            //Chuyển dữ liệu từ DataTable vào mảng đối tượng
+            for (int r = 0; r < tb.Rows.Count; r++)
+            {
+                DataRow dr = tb.Rows[r];
+                for (int c = 0; c < tb.Columns.Count; c++)
+
+                {
+                    arr[r, c] = dr[c];
+                }
+            }
+
+            //Thiết lập vùng điền dữ liệu
+            int rowStart = 4;
+            int columnStart = 1;
+            int rowEnd = rowStart + tb.Rows.Count - 1;
+            int columnEnd = tb.Columns.Count;
+            // Ô bắt đầu điền dữ liệu
+            excel.Range c1 = (excel.Range)oSheet.Cells[rowStart, columnStart];
+            // Ô kết thúc điền dữ liệu
+            excel.Range c2 = (excel.Range)oSheet.Cells[rowEnd, columnEnd];
+            // Lấy về vùng điền dữ liệu
+            excel.Range range = oSheet.get_Range(c1, c2);
+            //Điền dữ liệu vào vùng đã thiết lập
+            range.Value2 = arr;
+            // Kẻ viền
+            range.Borders.LineStyle = Microsoft.Office.Interop.Excel.Constants.xlSolid;
+            // Căn giữa cột STT
+            excel.Range c3 = (excel.Range)oSheet.Cells[rowEnd, columnStart];
+            excel.Range c4 = oSheet.get_Range(c1, c3);
+            oSheet.get_Range(c3, c4).HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+        }
+
+        private void btnExcel_Click(object sender, EventArgs e)
+        {
+            DateTime checkIn = Convert.ToDateTime(dtpkFromDate.Value);
+            DateTime checkOut = Convert.ToDateTime(dtpkToDate.Value);
+
+            SqlConnection connect = new SqlConnection(con);
+            connect.Open();
+            SqlCommand cmd = new SqlCommand("ThongKeHDReport", connect);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@checkIn", SqlDbType.DateTime).Value = checkIn;
+            cmd.Parameters.Add("@checkOut", SqlDbType.DateTime).Value = checkOut;
+            cmd.ExecuteNonQuery();
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = cmd;
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            cmd.Dispose();
+            connect.Close();
+            ExportExcel(ds.Tables[0], "Thống Kê Tài Chính");
+        }
+        #endregion
+
+
     }
 }
